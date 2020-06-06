99cda27247ca6bb5b0061124b00d0e5008f296af
Shubham510 <Shubham510@users.noreply.github.com>
2020-06-07 01:33:02 +0530

    Add Passport Local Authentication Strategy

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,12 +7,20 @@ module.exports.profile = function(req,res){
 }
 
 module.exports.signUp = function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_up',{
         title: " Todo | Sign Up"
     })
 }
 
 module.exports.signIn = function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_in',{
         title: " Todo | Sign In"
     })
@@ -42,5 +50,11 @@ module.exports.create = function(req,res){
 
 // sign in and create a session
 module.exports.createSession = function(req,res){
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function (req,res){
+    req.logout();
+    
     return res.redirect('/');
 }
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
-
+const MongoStore = require('connect-mongo')(session);
 
 app.use(express.urlencoded());
 
@@ -20,6 +20,7 @@ app.use(expressLayouts);
 app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
 
+//mongo store is used to store the session in the db
 app.use(session({
     name: 'todo',
     secret: 'Somethingtodo',
@@ -27,11 +28,22 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000*60*100)
-    }
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection:db,
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect=mongodb setup ok');
+        }
+    )
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
 // use express router
 
 app.use('/',require('./routes'));
