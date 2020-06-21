e089be6a088be07ca7adeffd8a45ee570f521777
Shubham510 <Shubham510@users.noreply.github.com>
2020-06-22 02:27:25 +0530

    Add option to delete post or comment

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post =require('../models/post')
+const Comment=require('../models/comment');
 
 module.exports.create = function(req,res){
      Post.create({
@@ -9,4 +10,21 @@ module.exports.create = function(req,res){
 
          return res.redirect('back');
      });
+}
+
+
+module.exports.destroy = function(req,res){
+    Post.findById(req.params.id, function(err,post){
+        if(err){console.log('Error in deleting post',err); return;}
+        // .id means converting the object id into string
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, function(err){
+                res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    })
 }
