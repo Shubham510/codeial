26e3af186cbf7e78a284d14bc3e0f4bb727bbd51
Shubham510 <Shubham510@users.noreply.github.com>
2020-06-21 11:47:01 +0530

    Allow only Authenticated users to post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,8 +3,20 @@ const Post = require('../models/post');
 
 module.exports.home = function(req,res){
     
-    return res.render('home',{
-        title: "Home"
-    });
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title: "Home| Codeial",
+    //         posts: posts
+    //     });
+    // });
+    
+
+    //populate user's name
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title: "Home| Codeial",
+            posts: posts
+        });
+    })
 }
 
