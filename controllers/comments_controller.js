ee09ae9356272ab650db4c7954b4902d431b51a4
Shubham510 <Shubham510@users.noreply.github.com>
2020-12-19 15:38:00 +0530

    Add jobs queue

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,8 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 const commentsMailer = require('../mailers/comments_mailer');
+const queue = require('../config/kue');
+const commentEmailWorker = require('../workers/comment_email_worker');
 
 module.exports.create = async function(req,res){
     try{
@@ -15,7 +17,15 @@ module.exports.create = async function(req,res){
             post.save();
             console.log('Post saved');
             comment = await comment.populate('user', 'name email').execPopulate();
-            commentsMailer.newComment(comment);
+            //commentsMailer.newComment(comment);
+            let job = queue.create('emails',comment).save(function(err){
+                if(err){
+                    console.log('Error in creating a queue', err);
+                    return;
+                }
+
+                console.log('Job enqueued',job.id);
+            });
             req.flash('success', 'Comment published!');
             res.redirect('/');
         }
